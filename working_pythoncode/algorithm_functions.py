f494873958ba406ba70e4958a554a1a944a7a150
Felix Pfreundtner <humanityhour@googlemail.com>
2015-05-22 16:29:04 +0200

    algorithm.main und algorithm_function hinzugefügt

---
+++ working_pythoncode/algorithm_functions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 16:25:01 2015
+
+@author: Felix Pfreundtner
+"""
+
---
+++ working_pythoncode/algorithm_main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 16:27:57 2015
+
+@author: Felix Pfreundtner
+"""
+
