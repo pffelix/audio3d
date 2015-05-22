b0ccac3e942d7e80082a8187c6df6a2e1ac3f033
Felix Pfreundtner <humanityhour@googlemail.com>
2015-05-22 18:34:15 +0200

    added some algorithm functions

--- working_pythoncode/algorithm_functions.py
+++ working_pythoncode/algorithm_functions.py
@@ -4,4 +4,29 @@ Created on Fri May 22 16:25:01 2015
 
 @author: Felix Pfreundtner
 """
+import math
+
+def create_standard_dict(gui_dict):
+      for item in gui_dict:
+          gui_dict[item]=[]
+      return gui_dict    
+
+def set_fft_param(output_fps, gui_dict, wave_param_common):
+    # Standard FFT block size colculation dependend on output_fps
+    fft_blocksize = 2**(round(math.log(wave_param_common[0]/output_fps, 2)))
+    fft_blocktime = fft_blocksize/wave_param_common[0]
+    output_fps_real = 1/fft_blocktime
+    return fft_blocksize, fft_blocktime, output_fps_real
+
+def initialze_wave_blockbeginend(gui_dict):
+    for item in gui_dict:
+          gui_dict[item]=[0,float(-1)]
+    return gui_dict
+
+def wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime):   
+    for item in wave_blockbeginend_dict:
+        wave_blockbeginend_dict[item][0]=wave_blockbeginend_dict[item][1] + 1
+        wave_blockbeginend_dict[item][1]=wave_blockbeginend_dict[item][1] + fft_blocktime*wave_param_dict[item][1] 
+    return wave_blockbeginend_dict
+
 
--- working_pythoncode/algorithm_main.py
+++ working_pythoncode/algorithm_main.py
@@ -5,3 +5,34 @@ Created on Fri May 22 16:27:57 2015
 @author: Felix Pfreundtner
 """
 
+import algorithm_functions
+import scipy.io.wavfile as wav
+
+
+#manuell eingegebene Werte, da noch kein Mockup verfügbar
+gui_dict={0: [90,2,"./audio/electrical_guitar_(44.1,16).wav"],
+          1: [270,1,"./audio/sine_1kHz_(44.1,16).wav"]
+         }
+         
+wave_param_dict={0: [970200, 44100,16],
+                 1: [220500, 441,16]
+                } 
+         
+            
+#Initialize FFT iteration after GUI call
+        
+#Standard samplerate, sampledepth         
+wave_param_common = [44100,16]  
+output_fps = 60                       
+iterationcounter = 0
+fft_blocksize, fft_blocktime, output_fps_real = algorithm_functions.set_fft_param(output_fps, gui_dict, wave_param_common)
+wave_blockbeginend_dict=algorithm_functions.initialze_wave_blockbeginend(gui_dict)
+
+
+#Run FFT iteration  
+
+#range of frames to be read in iteration from wav files (float numbers needed for interpolation between different frame rates)               
+wave_blockbeginend_dict=algorithm_functions.wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime)
+iterationcounter+=1
+          
+          
