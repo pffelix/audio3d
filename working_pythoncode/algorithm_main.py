08f7ac108b53e2a16d05c481d3f3a7e9655c62d6
Felix Pfreundtner <humanityhour@googlemail.com>
2015-05-22 22:13:24 +0200

    add agorithm function get_hrtf

--- working_pythoncode/algorithm_main.py
+++ working_pythoncode/algorithm_main.py
@@ -10,8 +10,8 @@ import scipy.io.wavfile as wav
 
 
 #manuell eingegebene Werte, da noch kein Mockup verfügbar
-gui_dict={0: [90,2,"./audio/electrical_guitar_(44.1,16).wav"],
-          1: [271,1,"./audio/sine_1kHz_(44.1,16).wav"]
+gui_dict={0: [4.9,2,"./audio/electrical_guitar_(44.1,16).wav"],
+          1: [182.7,1,"./audio/sine_1kHz_(44.1,16).wav"]
          }
          
 wave_param_dict={0: [970200, 44100,16],
@@ -21,11 +21,11 @@ wave_param_dict={0: [970200, 44100,16],
             
 #Initialize FFT iteration after GUI call
         
-#Standard samplerate, sampledepth         
+#Standard samplerate, sampledepth, output_frames_per_second         
 wave_param_common = [44100,16]  
 standard_dict=algorithm_functions.create_standard_dict(gui_dict)
-
-output_fps = 60                       
+output_fps = 60  
+                     
 iterationcounter = 0
 fft_blocksize, fft_blocktime, output_fps_real = algorithm_functions.set_fft_param(output_fps, wave_param_common)
 wave_blockbeginend_dict=algorithm_functions.initialze_wave_blockbeginend(standard_dict)
@@ -34,10 +34,13 @@ wave_blockbeginend_dict=algorithm_functions.initialze_wave_blockbeginend(standar
 #Run FFT iteration  
 
 #range of frames to be read in iteration from wav files (float numbers needed for interpolation between different frame rates)               
-hrtf_filenames=algorithm_functions.get_hrtf_filenames(standard_dict, gui_dict)
+wave_blockbeginend_dict = algorithm_functions.wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime)
+hrtf_filenames_dict = algorithm_functions.get_hrtf_filenames(standard_dict, gui_dict)
+hrtf_dict = algorithm_functions.get_hrtf(hrtf_filenames_dict, standard_dict, gui_dict)
 
-wave_blockbeginend_dict=algorithm_functions.wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime)
 iterationcounter+=1
+
+
 
 
           
