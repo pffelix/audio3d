581f9153ecab0e7f7098ff633dab1da0c5b4a966
H. Zhu <h.zhu@tum.de>
2015-05-23 17:02:05 +0200

    add speaker button implemented

--- working_pythoncode/gui_main_window.py
+++ working_pythoncode/gui_main_window.py
@@ -8,19 +8,32 @@ author: H. Zhu
 
 import sys
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 from gui_utils import *
 
 class MainWindow(QWidget):
 
+    speaker_list = []
+
     def __init__(self):
         super(MainWindow,self).__init__()
+        self.setAcceptDrops(True)
         self.init_ui()
 
     def init_ui(self):
 
+        def addspeaker():
+
+            new_speaker = Item()
+            new_speaker.setPos(0, 0)
+            self.speaker_list.append(new_speaker)
+            scene.addItem(new_speaker)
+            view.viewport().update()
+
         # set items
         speaker = Item()
         speaker.setPos(0, 0)
+        self.speaker_list.append(speaker)
 
         # set scene and view
         scene = Room()
@@ -28,12 +41,17 @@ class MainWindow(QWidget):
         scene.addItem(speaker)
         view = View(scene)
 
+        # set buttons
+        add_speaker_button = AddSpeakerButton()
+
         # set layout
         layout = QVBoxLayout()
         layout.addWidget(view)
-        layout.addWidget(QPushButton('Add Speaker'))
+        layout.addWidget(add_speaker_button)
         layout.addWidget(QPushButton('Reset'))
 
+        # connect signal and slots
+        add_speaker_button.clicked.connect(addspeaker)
         # set window
         self.setLayout(layout)
         self.resize(640,480)
