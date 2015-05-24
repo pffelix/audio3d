6c6ed10163d38d926da65cf4ba0aae526cde5083
Felix Pfreundtner <humanityhour@googlemail.com>
2015-05-24 16:50:23 +0200

    algorithm main while loop added

--- working_pythoncode/algorithm_functions.py
+++ working_pythoncode/algorithm_functions.py
@@ -18,8 +18,8 @@ import scipy.io.wavfile
 # @author: Felix Pfreundtner
 def create_standard_dict(gui_dict):
     standard_dict=deepcopy(gui_dict)
-    for source in standard_dict:
-        standard_dict[source]=[]
+    for sp in standard_dict:
+        standard_dict[sp]=[]
     return standard_dict   
     
 # @author: Felix Pfreundtner
@@ -49,76 +49,88 @@ def set_fft_param(output_fps, wave_param_common):
 # @author: Felix Pfreundtner
 def initialze_wave_blockbeginend(standard_dict, overlap, fft_blocktime, wave_param_dict):
     wave_blockbeginend_dict=deepcopy(standard_dict)   
-    for source in wave_blockbeginend_dict:
-          wave_blockbeginend_dict[source]=[-(fft_blocktime*wave_param_dict[source][1])*(1-overlap/100),0]
+    for sp in wave_blockbeginend_dict:
+          wave_blockbeginend_dict[sp]=[-(fft_blocktime*wave_param_dict[sp][1])*(1-overlap/100),0]
     return wave_blockbeginend_dict
     
 # @author: Felix Pfreundtner
 def wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime,overlap):   
-    for source in wave_blockbeginend_dict:
-        wave_blockbeginend_dict[source][0]=wave_blockbeginend_dict[source][0] + (fft_blocktime*wave_param_dict[source][1])*(1-overlap/100)
-        wave_blockbeginend_dict[source][1]=wave_blockbeginend_dict[source][0] + (fft_blocktime*wave_param_dict[source][1])
+    for sp in wave_blockbeginend_dict:
+        wave_blockbeginend_dict[sp][0]=wave_blockbeginend_dict[sp][0] + (fft_blocktime*wave_param_dict[sp][1])*(1-overlap/100)
+        wave_blockbeginend_dict[sp][1]=wave_blockbeginend_dict[sp][0] + (fft_blocktime*wave_param_dict[sp][1])
     return wave_blockbeginend_dict
 
 # @author: Felix Pfreundtner
 def get_hrtf_filenames(standard_dict, gui_dict):   
     hrtf_filenames_dict=deepcopy(standard_dict)
-    for source in hrtf_filenames_dict:
-        rounddifference = gui_dict[source][0] % 5
+    for sp in hrtf_filenames_dict:
+        rounddifference = gui_dict[sp][0] % 5
         if rounddifference == 0:
-            if gui_dict[source][0] <= 180:
-                azimuthangle=gui_dict[source][0]
+            if gui_dict[sp][0] <= 180:
+                azimuthangle=gui_dict[sp][0]
             else:
-                azimuthangle=360 - gui_dict[source][0]
+                azimuthangle=360 - gui_dict[sp][0]
         else:
-            if gui_dict[source][0] <= 180:
+            if gui_dict[sp][0] <= 180:
                 if rounddifference < 2.5:
-                    azimuthangle = round(gui_dict[source][0] - rounddifference) 
+                    azimuthangle = round(gui_dict[sp][0] - rounddifference) 
                 else:
-                    azimuthangle = round(gui_dict[source][0] + 5 - rounddifference)
+                    azimuthangle = round(gui_dict[sp][0] + 5 - rounddifference)
             else:  
                 if rounddifference < 2.5:
-                    azimuthangle = 360 - round(gui_dict[source][0] - rounddifference)
+                    azimuthangle = 360 - round(gui_dict[sp][0] - rounddifference)
                 else:    
-                    azimuthangle = 360 - round(gui_dict[source][0] + 5 - rounddifference)    
-        hrtf_filenames_dict[source] = ["./kemar/compact/elev0/H0e"+str(azimuthangle).zfill(3)+"a.wav"]
+                    azimuthangle = 360 - round(gui_dict[sp][0] + 5 - rounddifference)    
+        hrtf_filenames_dict[sp] = ["./kemar/compact/elev0/H0e"+str(azimuthangle).zfill(3)+"a.wav"]
     return hrtf_filenames_dict
 
 # @author: Felix Pfreundtner
 def get_hrtf(hrtf_filenames_dict, standard_dict, gui_dict):
     hrtf_dict=deepcopy(standard_dict)
-    for source in hrtf_filenames_dict:
-        for hrtf_filename in hrtf_filenames_dict[source]:
+    for sp in hrtf_filenames_dict:
+        for hrtf_filename in hrtf_filenames_dict[sp]:
             _, hrtf_input = scipy.io.wavfile.read(hrtf_filename)
-            if gui_dict[source][0] <= 180:
-                hrtf_dict[source]=hrtf_input
+            if gui_dict[sp][0] <= 180:
+                hrtf_dict[sp]=hrtf_input
             else:
                 hrtf_input[:,[0, 1]] = hrtf_input[:,[1, 0]]
-                hrtf_dict[source]=hrtf_input
+                hrtf_dict[sp]=hrtf_input
+                
     return hrtf_dict    
     
     
 # @author: Felix Pfreundtner
-def convolve(source_block, hrtf, fft_blocksize ):
+def convolve(sp_block, hrtf ):
     
     #hrtf_block = np.zeros((fft_blocksize-len(hrtf),),dtype='int16')
     #hrtf_block = np.concatenate((hrtf,hrtf_block))
-    convolved_block=fftconvolve(source_block, hrtf, mode='same')
+    convolved_block=fftconvolve(sp_block, hrtf, mode='same')
     return convolved_block
+    
+# @author: Felix Pfreundtner
+def apply_hamming_window(inputsignal):
+    hamming_window = [0.53836 - 0.46164*math.cos(2*math.pi*t/(len(inputsignal) - 1)) for t in range(len(inputsignal))]
+    hamming_window = np.asarray(hamming_window, dtype=np.float64)
+    inputsignal = inputsignal * hamming_window
+    return inputsignal
+    
 
 # @author: Felix Pfreundtner
 def bit_int(convolved_dict):
     convolved_dict_scaled={}
-    for source in convolved_dict:
-        convolved_dict_scaled[source] = np.empty((len(convolved_dict[source]), 2))
-        for left_right in range(2):
-            convolved_dict_scaled[source][:, left_right] = convolved_dict[source][:, left_right]/np.max(np.abs(convolved_dict[source][:, left_right])) * 32767
-        convolved_dict_scaled[source] = convolved_dict_scaled[source].astype(np.int16, copy=False)
+    for sp in convolved_dict:
+        convolved_dict_scaled[sp] = np.zeros((len(convolved_dict[sp]), 2), dtype=np.int16)
+        for l_r in range(2):
+            maximum_value=np.max(np.abs(convolved_dict[sp][:, l_r]))
+            if maximum_value != 0:
+                convolved_dict_scaled[sp][:, l_r] = convolved_dict[sp][:, l_r]/maximum_value * 32767
+                convolved_dict_scaled[sp] = convolved_dict_scaled[sp].astype(np.int16, copy=False)
   
     return convolved_dict_scaled
 
+
 # @author: Felix Pfreundtner
 def writebinauraloutput(convolved_dict_scaled, wave_param_common, gui_dict):
-    for source in convolved_dict_scaled:
-        scipy.io.wavfile.write(gui_dict[source][2] + "binauraloutput.wav" , wave_param_common[0], convolved_dict_scaled[source])    
+    for sp in convolved_dict_scaled:
+        scipy.io.wavfile.write(gui_dict[sp][2] + "binauraloutput.wav" , wave_param_common[0], convolved_dict_scaled[sp])    
         
--- working_pythoncode/algorithm_main.py
+++ working_pythoncode/algorithm_main.py
@@ -13,30 +13,41 @@ import scipy.io.wavfile
 
 # GUI mockup
 gui_dict={0: [272,1,"./audio/electrical_guitar_(44.1,16).wav"],
-          1: [192.45,1,"./audio/sine_1kHz_(44.1,16).wav"]
+          1: [2.45,1,"./audio/sine_1kHz_(44.1,16).wav"]
          }
 # Algorithm function get_wave_param mockup         
 wave_param_dict={0: [970200, 44100,16],
                  1: [220500, 44100,16]
                 } 
 # Algorithm function get_fft_block mockup         
-fft_block_complete_signal_dict={} 
-fft_block_dict={}             
-_, fft_block_complete_signal_dict[0] = scipy.io.wavfile.read(gui_dict[0][2])              
-_, fft_block_complete_signal_dict[1] = scipy.io.wavfile.read(gui_dict[1][2])       
+signal_dict={} 
+sp_block_dict={}             
+_, signal_dict[0] = scipy.io.wavfile.read(gui_dict[0][2])              
+_, signal_dict[1] = scipy.io.wavfile.read(gui_dict[1][2])       
 
  
  
 #Initialize FFT iteration after GUI call
         
+
+# Initialize Dictionarys
+standard_dict=alf.create_standard_dict(gui_dict)
+convolved_dict=deepcopy(standard_dict)
+for sp in convolved_dict:
+    convolved_dict[sp] = np.empty((len(signal_dict[sp]), 2))
+    
+continue_output=deepcopy(standard_dict)
+continue_output_list=deepcopy(standard_dict)
+for sp in convolved_dict:
+    continue_output[sp] = True
+
 #Standard samplerate, sampledepth, output_frames_per_second         
 wave_param_common = [44100,16]  
-standard_dict=alf.create_standard_dict(gui_dict)
-
+# Determine number of output frames per second
 output_fps = 60  
 #Overlap FFT in Prozent
 overlap=0         
-iterationcounter = 200
+iterationcounter = 1
 
 fft_blocksize, fft_blocktime, output_fps_real = alf.set_fft_param(output_fps, wave_param_common)
 wave_blockbeginend_dict = alf.initialze_wave_blockbeginend(standard_dict, overlap, fft_blocktime, wave_param_dict)
@@ -44,35 +55,43 @@ wave_blockbeginend_dict = alf.initialze_wave_blockbeginend(standard_dict, overla
 
 
 # Run convolution iteration  
-while iterationcounter <=215:
-    # Get current hrtf file dependend on input angle for every source
+while any(continue_output.values()) == True:
+    
+    # Get current hrtf file dependend on input angle for every sp
     hrtf_filenames_dict = alf.get_hrtf_filenames(standard_dict, gui_dict)
     hrtf_dict = alf.get_hrtf(hrtf_filenames_dict, standard_dict, gui_dict)
-
-
-    # Read wave samples with fft block framesize for every source
-
     # range of frames to be read in iteration from wav files (float numbers needed for adding the correct framesizes to the next iteration)               
-    # @matthias: for reading the wave file from sample a to b you need to round the constraints from wave_blockbeginend_dict{sp#,[ a, b]: -> int(alf.normal_round(a)) and int(alf.normal_round(b))
     wave_blockbeginend_dict = alf.wave_blockbeginend(wave_blockbeginend_dict, wave_param_dict, fft_blocktime, overlap)
-
-    # convolve hrtfs with sources
-    if iterationcounter == 1:
-        convolved_dict={}
-        convolved_block_dict={}  
-
-    for source in fft_block_complete_signal_dict:
-        # Algorithm function get_fft_block mockup  
-        fft_block_dict[source] = fft_block_complete_signal_dict[source][int(alf.normal_round(wave_blockbeginend_dict[source][0])):int(alf.normal_round(wave_blockbeginend_dict[source][1]))]
+    convolved_block_dict={}
     
-        # convolve hrtfs with sources
-        if iterationcounter == 1:
-            convolved_dict[source] = np.empty((len(fft_block_complete_signal_dict[source]), 2))
-        convolved_block_dict[source]=np.zeros((fft_blocksize, 2))
-        for left_right in range(2):
-            convolved_block_dict[source][:, left_right] = alf.convolve(fft_block_dict[source], hrtf_dict[source][:,left_right], fft_blocksize)
-            convolved_dict[source][wave_blockbeginend_dict[source][0]:wave_blockbeginend_dict[source][1], left_right] = convolved_block_dict[source][:, left_right]
-
+    for sp in gui_dict:
+        convolved_block_dict[sp]=np.zeros((fft_blocksize, 2))
+        
+        # check wheter this block is last block in speaker audio file, set ending of the block to last sample in speaker audio file
+        if wave_blockbeginend_dict[sp][1] > wave_param_dict[sp][0]-1:
+            wave_blockbeginend_dict[sp][1] = wave_param_dict[sp][0]-1
+        
+        # if speaker audio file still has unplayed samples start convolution 
+        if continue_output[sp] == True:
+            # Read wave samples with fft block framesize for every speaker
+            sp_block_dict[sp] = signal_dict[sp][int(alf.normal_round(wave_blockbeginend_dict[sp][0])):int(alf.normal_round(wave_blockbeginend_dict[sp][1]))]
+            # for the left an right ear channel
+            for l_r in range(2):
+                # convolve hrtf with speaker block input
+                convolved_block_dict[sp][0:len(sp_block_dict[sp]), l_r] = alf.convolve(sp_block_dict[sp], hrtf_dict[sp][:,l_r])
+                # apply hamming window to binaural block ouptut
+                convolved_block_dict[sp][:, l_r]= alf.apply_hamming_window(convolved_block_dict[sp][:, l_r])
+                # add speaker binaural block output to a iterative time based output array
+                convolved_dict[sp][int(alf.normal_round(wave_blockbeginend_dict[sp][0])):int(alf.normal_round(wave_blockbeginend_dict[sp][1])), :] += convolved_block_dict[sp][:, :]
+        
+        # check wheter this block is last block in speaker audio file and stop convolution of speaker audio file
+        if wave_blockbeginend_dict[sp][1] > wave_param_dict[sp][0]-1:
+            continue_output[sp] == False
+            
+        # record how long each speaker audio file was convoluted    
+        continue_output_list[sp].append(continue_output[sp])
+        
+        
     iterationcounter+=1
 
     
