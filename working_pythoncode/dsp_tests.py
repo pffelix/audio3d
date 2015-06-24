8ca81166ac79bdc432a16811d73827407824ee0f
Matthias <matthias.lederle@tum.de>
2015-06-24 17:19:42 +0200

    2nd rnd-test

--- working_pythoncode/dsp_tests.py
+++ working_pythoncode/dsp_tests.py
@@ -22,16 +22,38 @@ DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
                                      DspIn_TestObj.hopsize,
                                      DspIn_TestObj, gui_pause_mockup)
 
-
 class DspTests(unittest.TestCase):
 
+    # def __init__(self):
+    #     self.DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup,
+    #                                       gui_settings_dict_mockup)
+    #     self.DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
+    #                                          DspIn_TestObj.fft_blocksize,
+    #                                          DspIn_TestObj.sp_blocksize,
+    #                                          DspIn_TestObj.hopsize,
+    #                                          DspIn_TestObj, gui_pause_mockup)
+
+    def test_rnd_int(self):
+        val = 1.3
+        sol = 2
+        res = DspIn_TestObj.rnd(val)
+        error_msg = "test_rnd_int failed!"
+        self.assertEqual(res, sol, msg=error_msg)
+
     def test_rnd(self):
-        print("B")
-        value = 2.4
-        res = DspIn_TestObj.rnd(value)
-        error_msg = "test_rnd failed: Function does not properly round 2.55 " \
-                    "to 3"
-        self.assertEqual(res, 3, msg=error_msg)
+        i = 0
+        value = [2.55, 7.9, (2 / 3), 0.5, 0.00001, 500.1, -80.1, -1.4142, -9.5]
+        res = [None]*len(value)
+        sol = [2, 8, 1, 1, 0, 500, -80, -1, -10]
+        # value = []
+        #value[0] = 2
+        # sol = 2
+        while i < len(value):
+            #res[i] = res.append(DspIn_TestObj.rnd(value[i]))
+            res[i] = DspIn_TestObj.rnd(value[i])
+            i += 1
+        error_msg = "test_rnd failed!"
+        self.assertListEqual(res, sol, msg=error_msg)
 
 if __name__ == '__main__':
     unittest.main()
