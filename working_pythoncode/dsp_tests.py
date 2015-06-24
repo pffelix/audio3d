5a31811b58d9964f6f64b4f705fac15cf423de34
Felix Pfreundtner <humanityhour@googlemail.com>
2015-06-24 14:27:29 +0200

    fixed errors in test class -> @matthias: initialzation of dsp objects with an constructor doesn't work if you inherit from unittest.Testcase class without calling super class-> easy circumvent: initiliaze all objects as global variabels

--- working_pythoncode/dsp_tests.py
+++ working_pythoncode/dsp_tests.py
@@ -1,31 +1,35 @@
 __author__ = 'Matthias'
-
 import unittest
 import dsp_in
 import dsp_out
-import mockup
 
+gui_dict_mockup = {0: [90, 0, "./audio_in/sine_1kHz_(44.1,1,"
+                               "16).wav", False]
+                 #0: [120, 1, "./audio_in/electrical_guitar_(44.1,1,16).wav", True]
+                 #0: [0, 1, "./audio_in/synthesizer_(44.1,1,16).wav",
+                 #  True]
+                  }
+gui_settings_dict_mockup = {"hrtf_database": "kemar_normal_ear",
+         "inverse_filter_active": True,
+         "bufferblocks": 5}
+gui_stop_mockup = False
+gui_pause_mockup = False
+
+DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup,gui_settings_dict_mockup)
+DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
+                                     DspIn_TestObj.fft_blocksize,
+                                     DspIn_TestObj.sp_blocksize,
+                                     DspIn_TestObj.hopsize,
+                                     DspIn_TestObj, gui_pause_mockup)
 
 class DspTests(unittest.TestCase):
-
-    def __init__(self):
-        self.DspIn_TestObj = dsp_in.DspIn(gui_dict_init, gui_settings_dict_init)
-        self.DspOut_TestObj = dsp_out.DspOut(gui_dict_init,
-                                             self.DspIn_Object.fft_blocksize,
-                                             self.DspIn_Object.sp_blocksize,
-                                             self.DspIn_Object.hopsize,
-                                             gui_stop_init, gui_pause_init)
-        self.mockup = mockup.gui_dict_mockup
-        print("A")
-
     def test_rnd(self):
         print("B")
-        value = 2.55
-        res = self.DspIn_TestObj.rnd(value)
+        value = 2.4
+        res = DspIn_TestObj.rnd(value)
         error_msg = "test_rnd failed: Function does not properly round 2.55 " \
                     "to 3"
         self.assertEqual(res, 3, msg=error_msg)
-    print("C")
-#if __name__ == '__main__':
+
+if __name__ == '__main__':
     unittest.main()
-    print("D")
