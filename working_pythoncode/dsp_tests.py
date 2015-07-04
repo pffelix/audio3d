0b7babe451ba7ecfa517130bf2648dfe2d3a8ac1
Felix Pfreundtner <humanityhour@googlemail.com>
2015-07-04 15:07:43 +0200

    added constructor and self namespace in dsp_test.py

--- working_pythoncode/dsp_tests.py
+++ working_pythoncode/dsp_tests.py
@@ -6,6 +6,7 @@ from unittest.mock import MagicMock
 import numpy as np
 import scipy.io.wavfile
 
+# global Namespace
 gui_dict_mockup = \
     {
         #0: [90, 0, "./audio_in/sine_1kHz_(44.1,1,16).wav", False]
@@ -16,43 +17,58 @@ gui_dict_mockup = \
 gui_settings_dict_mockup = {"hrtf_database": "kemar_normal_ear",
                             "inverse_filter_active": True,
                             "bufferblocks": 5}
-gui_stop_mockup = False
-gui_pause_mockup = False
-wave_param_common = [44100, 16]
-hrtf_blocksize = 513
-fft_blocksize = 1024
-sp_blocksize = 512
-sp_blocktime = 0.011609977324263039
-overlap = 0.5
-hopsize = 256
+
+# @ Matthias: I uncommented this params as they are now in self
+# namespace initialized in constructor
+# DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup, gui_settings_dict_mockup)
+# DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
+                                  # DspIn_TestObj.fft_blocksize,
+                                  # DspIn_TestObj.sp_blocksize,
+                                  # DspIn_TestObj.hopsize,
+                                  # DspIn_TestObj, gui_pause_mockup)
+# "bufferblocks": 5
+# gui_stop_mockup = False
+# gui_pause_mockup = False
+# wave_param_common = [44100, 16]
+# hrtf_blocksize = 513
+# fft_blocksize = 1024
+# sp_blocksize = 512
+# sp_blocktime = 0.011609977324263039
+# overlap = 0.5
+# hopsize = 256
+
+
 # only for test_get_sp (only testable with elecguitar)
 total_no_samples_elecgui = 970200
 sp = 0
 scipy_sp_dict = {}
 
 
-DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup, gui_settings_dict_mockup)
-DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
-                                DspIn_TestObj.fft_blocksize,
-                                DspIn_TestObj.sp_blocksize,
-                                DspIn_TestObj.hopsize,
-                                DspIn_TestObj, gui_pause_mockup)
+
 #following calculation of block_begin_end must be equal to the one in the
 # function
 block_begin_end = np.zeros((2,), dtype=np.int16)
 
 
 class DspTests(unittest.TestCase):
-    # def __init__(self):
-    #     self.DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup,
-    #                                       gui_settings_dict_mockup)
-    #     self.DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
-    #                                          DspIn_TestObj.fft_blocksize,
-    #                                          DspIn_TestObj.sp_blocksize,
-    #                                          DspIn_TestObj.hopsize,
-    #                                          DspIn_TestObj, gui_pause_mockup)
+    def __init__(self, *args, **kwargs):
+        # self Namespace
+        # calling the constructor of the super class
+        super(DspTests, self).__init__(*args, **kwargs)
+        self.gui_dict = gui_dict_mockup
+        self.gui_settings_dict = gui_settings_dict_mockup
+        self.gui_stop = False
+        self.gui_pause = False
+        self.DspIn_TestObj = dsp_in.DspIn(gui_dict_mockup,
+                                          gui_settings_dict_mockup)
+        self.DspOut_TestObj = dsp_out.DspOut(gui_dict_mockup,
+                                             self.DspIn_TestObj.fft_blocksize,
+                                             self.DspIn_TestObj.sp_blocksize,
+                                             self.DspIn_TestObj.hopsize,
+                                             self.DspIn_TestObj,
+                                             self.gui_pause)
 
-    ## @brief Tests rnd for one particular number.
+    # @brief Tests rnd for one particular number.
     def test_rnd_int(self):
         val = 1.9
         sol = 2
@@ -60,7 +76,7 @@ class DspTests(unittest.TestCase):
         error_msg = "test_rnd_int failed!"
         self.assertEqual(res, sol, msg=error_msg)
 
-    ## @brief Tests rnd for a list of numbers at the same time.
+    # @brief Tests rnd for a list of numbers at the same time.
     def test_rnd(self):
         i = 0
         value = [2.55, 7.9, (2 / 3), 0.5, 0.00001, 500.1, -80.1, -1.4142, -9.5]
@@ -76,7 +92,7 @@ class DspTests(unittest.TestCase):
         error_msg = "test_rnd failed!"
         self.assertListEqual(res, sol, msg=error_msg)
 
-    ## @brief Tests equality of the values at position 2 and 200 of the
+    # @brief Tests equality of the values at position 2 and 200 of the
     #  hamming-window.
     def test_hann_window(self):
         sol_hannwin_2 = 0.00014997
@@ -86,22 +102,24 @@ class DspTests(unittest.TestCase):
         self.assertAlmostEqual(res[2], sol_hannwin_2, 5, msg=errmsg)
         self.assertAlmostEqual(res[200], sol_hannwin_200, 5, msg=errmsg)
 
-    ## @brief Tests get_block_param by comparing two lists.
+    # @brief Tests get_block_param by comparing two lists.
     def test_get_block_param(self):
         sol = [512, 0.011609977324263039, 0.5, 256]
         res = [None] * 3
         res[0: 3] = DspIn_TestObj.get_block_param(
-            wave_param_common, hrtf_blocksize, fft_blocksize)
+            self.DspIn_TestObj.wave_param_common,
+            self.DspIn_TestObj.hrtf_blocksize,
+            self.DspIn_TestObj.fft_blocksize)
         errmsg = "Function get_block_param (in DspIn) doesn't work properly"
         self.assertListEqual(res, sol, msg=errmsg)
 
-    ## @brief Tests the values of init_block_begin_end on symmetry to 0
+    # @brief Tests the values of init_block_begin_end on symmetry to 0
     def test_init_set_block_begin_end(self):
         res = DspIn_TestObj.init_set_block_begin_end(gui_dict_mockup)
         errmsg = "The entries in init_block_begin_end are not symmetric to 0"
         self.assertTrue(abs(res[0]) == abs(res[1]), msg=errmsg)
 
-    ## @brief Tests the set_block_begin_end function for correctness.
+    # @brief Tests the set_block_begin_end function for correctness.
     # Due to the fact, that the function does not return anything, it has to
     # be copied manually to the section between the ##### below.
     def test_set_block_begin_end(self):
@@ -112,10 +130,13 @@ class DspTests(unittest.TestCase):
             # Between ##### and ##### has to be the same code as in
             # set_block_begin_end-function
             #####
-            block_begin_end[0] += int(sp_blocksize * (1 - overlap))
-            block_begin_end[1] += int(sp_blocksize * (1 - overlap))
+            block_begin_end[0] += int(self.DspIn_TestObj.sp_blocksize * (1 -
+                                      self.DspIn_TestObj.overlap))
+            block_begin_end[1] += int(self.DspIn_TestObj.sp_blocksize * (1 -
+                                      self.DspIn_TestObj.overlap))
             #####
-            if block_begin_end[1] - block_begin_end[0] == sp_blocksize:
+            if block_begin_end[1] - block_begin_end[0] == \
+                    self.DspIn_TestObj.sp_blocksize:
                 bool = True
             else:
                 bool = False
@@ -124,16 +145,16 @@ class DspTests(unittest.TestCase):
             i += 1
         self.assertTrue(truelist, msg=errmsg)
 
-    ## @brief Tests get_sp_block.
+    # @brief Tests get_sp_block.
     # def test_get_sp_block(self):
     #
     #     res = DspIn_TestObj.get_sp_block(0)
     #     #np.zeros((
-    #         #self.sp_blocksize,), dtype=np.int16))
+    #         #self.DspIn_TestObj.sp_blocksize,), dtype=np.int16))
     #     self.assertTrue(res)
 
-    ## @brief Compare own read-function to scipy-function-results.
-    #Skip Test for all files besides the electrical guitar
+    # @brief Compare own read-function to scipy-function-results.
+    # Skip Test for all files besides the electrical guitar
     @unittest.skipUnless(gui_dict_mockup[0][2] ==
                          "./audio_in/electrical_guitar_(44.1,1,16).wav",
                          "Otherwise total_no_of_samples is wrong")
@@ -148,9 +169,11 @@ class DspTests(unittest.TestCase):
             lenarray = len(scipy_sp_dict_raw[sp])
             # append zeros to scipy_sp_dict_raw to reach that output is
             # divideable by sp_blocksize
-            if lenarray % sp_blocksize != 0:
-                scipy_sp_dict[sp] = np.zeros((lenarray + sp_blocksize -
-                                              lenarray % sp_blocksize, ),
+            if lenarray % self.DspIn_TestObj.sp_blocksize != 0:
+                scipy_sp_dict[sp] = np.zeros((lenarray +
+                                              self.DspIn_TestObj.sp_blocksize -
+                                              lenarray %
+                                              self.DspIn_TestObj.sp_blocksize, ),
                                              dtype=np.int16)
                 scipy_sp_dict[sp][0:lenarray, ] = scipy_sp_dict_raw[sp]
             else:
@@ -158,7 +181,7 @@ class DspTests(unittest.TestCase):
         sol = scipy_sp_dict
         res = DspIn_TestObj.get_sp(gui_dict_mockup)
         errmsg = "get_sp doesn't get same values as scipy function"
-        #Following while-loop only for bug-fixes:
+        # Following while-loop only for bug-fixes:
         # i = 0
         # while i < 970200:
         #     if sol[0][i] == res[0][i]:
