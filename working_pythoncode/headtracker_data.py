e83f7371487e916508fd38ba77c5abfd43d9f8b3
mh <mh@heissa.de>
2015-07-20 06:20:31 +0000

    sphinx style comments

--- working_pythoncode/headtracker_data.py
+++ working_pythoncode/headtracker_data.py
@@ -1,12 +1,15 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Jun 23 16:46:54 2015
-
-@author: mh
+Handling of headtracker data from DT2 class
+author: H. Zhu, M. Heiss
 """
 
 
 def clean_and_split(line):
+    """
+    H2 -- clean_and_split
+    ===================
+    **Formatting of headtracker data output from DT2.**
+    """
     strings = str(line)  # everything as a string
     wo_brackets = strings.replace("]", "")  # removes ] brackets
     splitted = wo_brackets.split("[")  # creates LIST with items between [
@@ -14,12 +17,23 @@ def clean_and_split(line):
 
 
 def string_to_float(s):
+    """
+    H2 -- string_to_float
+    ===================
+    **Formatting of headtracker data output from DT2.**
+    """
     splitted = s.split(" ")
     coor_float = [float(i) for i in splitted]
     return coor_float
 
 
 def azimuth_angle(data):
+    """
+    H2 -- azimuth_angle
+    ===================
+    **Extraction of azimuthal angle from headtracker data output and
+    conversion to defined coordinate system.**
+    """
     data_list = clean_and_split(data)  # returns list of string objects
     coor = data_list[3]  # returns the coordinate entry [x y z polar azimuth]
     coor_float = string_to_float(coor)  # converges strings to floats
