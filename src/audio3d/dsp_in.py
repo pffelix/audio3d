bf59b1c96bd10d7736fe2610c073e2bd93e9e34b
Matthias <matthias.lederle@tum.de>
2015-07-21 11:40:15 +0200

    documentation

--- src/audio3d/dsp_in.py
+++ src/audio3d/dsp_in.py
@@ -84,13 +84,18 @@ class DspIn:
 
     def rnd(self, value):
         """
-        H2 -- This is a header of 2nd level text
+        H2 -- rnd
         ===================
+        *Rounds numbers arithmetically.*
+
         This function does a normal school arithmetic round (choose lower
         int until .4 and higher int from .5 on) and returns the rounded
         value. It is NOT equal to pythons round() method.
+
         Return values:
+
         * value: Is the rounded int of any input number.
+
         Author: Felix Pfreundtner
         """
         if value >= 0:
@@ -109,10 +114,12 @@ class DspIn:
         """
         H2 -- build_hann_window
         ===================
-        Calculate and construct the hann window in dependency of
-        sp_blocksize.
+        *Calculate and construct the hann window in dependency of
+        sp_blocksize.*
+
         Return values:
         * hann_window: Numpy-array of the length of sp_blocksize
+
         Author: Felix Pfreundtner
         """
         x = sp_blocksize
@@ -125,15 +132,20 @@ class DspIn:
         """
         H2 -- get_block_param
         ===================
+        *Calculates blocksize and blocktime.*
+
         This method uses the parameters of the input wav-file and the
         blocksizes of the fft and the hrtf to calculate the blocksize and
         blocktime needed from the speaker.
-        Return values
+
+        Return values:
+
         * sp_blocksize: Amount of samples taken from the input file per block
         * sp_blocktime: Time it takes to play one block in [s]
         * overlap: overlap between two binaural output blocks in decimal
             value [calculated default is 0.5]
-        Author Felix Pfreundtner
+
+        Author: Felix Pfreundtner
         """
         sp_blocksize = self.fft_blocksize - self.hrtf_blocksize + 1
         sp_blocktime = sp_blocksize / self.samplerate
@@ -147,8 +159,8 @@ class DspIn:
         """
         H2 -- init_set_block_begin_end
         ===================
-        **Initializes a list with the number of the first and last sample
-        of the first block**
+        *Initializes a list with the number of the first and last sample
+        of the first block.*
 
         This function calculates a list called block_begin_end containing
         two elements: [0] is the first sample of the first block, [1] is
@@ -156,7 +168,8 @@ class DspIn:
         will then be calculated by the set_block_begin_end-function.
 
         Return value:
-        *block_begin_end: List of the first ([0]) and last ([0]) sample of
+
+        * block_begin_end: List of the first ([0]) and last ([0]) sample of
                           the FIRST block.
 
         Author: Felix Pfreundtner
@@ -169,8 +182,8 @@ class DspIn:
         """
         H2 -- set_block_begin_end
         ===================
-        **Every while-loop the number of the first and last sample is
-        calculated**
+        *Every while-loop the number of the first and last sample is
+        calculated.*
 
         This function replaces the values set by the
         init_set_block_begin_end-function every while-loop according to the
@@ -186,13 +199,14 @@ class DspIn:
         """
         H2 -- get_hrtf_param
         ===================
-        **Get all parameters for the hrtf set by the settings in gui**
+        *Get all parameters for the hrtf set by the settings in gui.*
 
         This function calculates all necessary parameters of the hrtf to be
         later able to get the correct hrtf-files for the convolution with
         the speaker-file signal.
 
         Return values:
+
         * hrtf_blocksize: Simply set to default value 513 since
         fft_blocksize is defaulted to 1024
         * kemar_inverse_filter: Boolean value that tells whether check-box
@@ -245,15 +259,14 @@ class DspIn:
             kemar_inverse_filter, kemar_inverse_filter_fft, \
             kemar_inverse_filter_active
 
-    # @brief Preloads all hrtf Files
-    # @author Felix Pfreundtner
     def read_hrtf_database(self):
         """
         H2 -- read_hrtf_database
         ===================
-        **Preloads all hrtf Files**
+        *Preloads all hrtf Files.*
 
         Return values:
+
         * hrtf_database: A numpy array that contains either the
         kemar_normal_ear or the kemar_full_ear HRTFs.
 
@@ -306,10 +319,11 @@ class DspIn:
         """
         H2 -- hrtf_database_fft
         ===================
-        **Converts the whole HRTF-database in frequency domain**
+        *Converts the whole HRTF-database in frequency domain.*
 
         Return values:
-        *hrtf_database_fft: The HRTF database converted to frequency domain.
+
+        * hrtf_database_fft: The HRTF database converted to frequency domain.
 
         Author: Felix Pfreundtner
         """
@@ -329,8 +343,8 @@ class DspIn:
         """
         H2 -- init_read_sp
         ===================
-        **Get 10 important parameters of the files to be played by the
-        get_block_function**
+        *Get 10 important parameters of the files to be played by the
+        get_block_function.*
 
         This method gets all important data from the .wav files that will
         be played by the speakers. The output is another list called sp_param,
@@ -338,7 +352,7 @@ class DspIn:
         by the gui_sp.
 
         Return values:
-        sp_param: Returns a list containing 10 properties of each
+        * sp_param: Returns a list containing 10 properties of each
         speaker-file.
 
         Author: Matthias Lederle
@@ -458,7 +472,7 @@ class DspIn:
         """
         H2 -- read_sp
         ===================
-        **Reads one block of samples**
+        *Reads one block of samples.*
 
         This method reads a block of samples of a speaker-.wav-file and
         writes in a numpyarray sp_block[sp] (containing one 16-bit-int
@@ -495,7 +509,7 @@ class DspIn:
         """
         H2 -- get_hrtf_block_fft
         ===================
-        **Gets and reads the correct hrtf_file from database**
+        *Gets and reads the correct hrtf_file from database.*
 
         Author: Felix Pfreundtner
         """
@@ -542,10 +556,11 @@ class DspIn:
         """
         H2 -- get_sp_block
         ===================
-        **Gets the current block for the speaker.**
+        *Gets the current block for the speaker.*
 
         Return value:
-        *Continue input: Boolean that tells whether its the last block of
+
+        * Continue input: Boolean that tells whether its the last block of
         the file or not
 
         Author: Matthias Lederle
@@ -568,7 +583,7 @@ class DspIn:
         """
         H2 -- get_hrtf_block_fft
         ===================
-        **Gets and reads the correct hrtf_file from database**
+        *Gets and reads the correct hrtf_file from database.*
 
         If the input-flag normalize_flag_sp is True, measure the maximum
         amplitude occurring in the .wav-file. After that, reduce all
@@ -594,7 +609,7 @@ class DspIn:
         """
         H2 -- apply_window_on_sp_block
         ===================
-        **Gets and reads the correct hrtf_file from database**
+        *Gets and reads the correct hrtf_file from database.*
 
         If the input-flag normalize_flag_sp is True, measure the maximum
         amplitude occurring in the .wav-file. After that, reduce all
@@ -611,6 +626,8 @@ class DspIn:
         """
         H2 -- set_fftfreq
         ===================
+        *Sets the frequency of the Fast-Fourier-Transformation.*
+
         Author: Felix Pfreundtner
         """
         freq_spacing = samplerate / fft_blocksize
@@ -628,7 +645,7 @@ class DspIn:
         """
         H2 -- fft_convolution
         ===================
-        **Function convolves hrtf and data of the music file**
+        *Function convolves hrtf and data of the music file.*
 
         Function takes one hrtf block and one data block (their size is
         defined by fft_blocksize), normalizes their values to int16-signals
